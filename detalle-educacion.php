ec20b4315ec139ef4d9f36b709bb9021a581ee90
Asistente de tecnologias <asistentetecnologias@cooperativa15deagosto.fin.ec>
2026-09-24 16:01:41 -0500

    se rediseño la pagina detalle-educacion.php

--- detalle-educacion.php
+++ detalle-educacion.php
@@ -1,35 +1,43 @@
 <?php
 require './controler/conexion.php';
 include './fn/fn-credito.php';
-require './funciones/fn-index.php'; require_once './funciones/fn-utilidades.php';
+require './funciones/fn-index.php';
+require_once './funciones/fn-utilidades.php';
 $con = new Conecciones();
 $fncredito = new Fn_credito();
 $fnindex = new Fn_index();
-$id = $_GET['id'];
+
+// CAPTURA DINÁMICA: Atrapamos el ID que viaja en la URL
+$id = isset($_GET['id']) ? intval($_GET['id']) : 14;
 $deteducafinan = $fnindex->fnindex_reducacion_financiera_x($id);
+
+// Saneamos de forma matemática la codificación para rescatar las tildes del título
+$titulo_articulo = !empty($deteducafinan[0]['titulo_edfi']) ? arreglar_mojibake($deteducafinan[0]['titulo_edfi']) : 'Artículo Educativo';
+$resumen_articulo = !empty($deteducafinan[0]['resumen_edfi']) ? arreglar_mojibake($deteducafinan[0]['resumen_edfi']) : '';
+$imagen_articulo = !empty($deteducafinan[0]['imagen_edfi']) ? $deteducafinan[0]['imagen_edfi'] : 'blog-img21.png';
+$fecha_articulo = !empty($deteducafinan[0]['fecha_edfi']) ? $deteducafinan[0]['fecha_edfi'] : 'Actualizado';
+$contenido_articulo = !empty($deteducafinan[0]['descripcion_edfi']) ? arreglar_mojibake($deteducafinan[0]['descripcion_edfi']) : '';
+
+$tituloPagina = $titulo_articulo;
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title> <?php echo arreglar_mojibake(utf8_encode($deteducafinan[0]['titulo_edfi'])) ?> - 15 de Agosto Cooperativa de Ahorro y Crédito</title>
-
-        <!--=====FAB ICON=======-->
+        <title><?php echo $titulo_articulo; ?> - COAC 15 de Agosto</title>
         <link rel="shortcut icon" href="assets/img/favicon-coop.png" type="image/x-icon">
-
         <?php include "head-v2.php"; ?>
     </head>
-    <body class="v2">
+    <body class="v2 bg-white text-neutral-800">
 
-        <!--===== PRELOADER STARTS =======-->
+        <!--===== PRELOADER =======-->
         <div class="preloader">
             <img src="assets/img/logo/logo2.png">
             <div class="loader"></div>
         </div>
-        <!--===== PRELOADER ENDS =======-->
 
-        <!--===== PROGRESS STARTS=======-->
+        <!--===== PROGRESS =======-->
         <div class="paginacontainer">
             <div class="progress-wrap">
                 <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
@@ -37,135 +45,104 @@ $deteducafinan = $fnindex->fnindex_reducacion_financiera_x($id);
                 </svg>
             </div>
         </div>
-        <!--===== PROGRESS ENDS=======-->
 
-        <!--=====HEADER START=======-->
         <header class="homepage2-body">
             <?php include 'header.php'; ?>
         </header>
-        <!--=====HEADER END =======-->
 
-        
-        <!--===== HERO AREA STARTS =======-->
-        <div class="inner-pages-section-area" style="background-image: url(assets/img/all-images/bg/bg-header-002.png); background-position: center; background-repeat: no-repeat; background-size: cover;">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12 m-auto">
-                        <div class="inner-header text-center">
-                            <h2 style="font-size: 65px;"><?php echo arreglar_mojibake(utf8_encode($deteducafinan[0]['titulo_edfi'])) ?></h2>
-                            <div class="space24"></div>
-                            <a href="index.php">Inicio <i class="fa-solid fa-angle-right"></i> <span><?php echo arreglar_mojibake(utf8_encode($deteducafinan[0]['titulo_edfi'])) ?></span></a>
-                        </div>
+        <!-- 1. LA FRANJA ROJA DE SIEMPRE: Gradiente Rojo Corporativo Unificado -->
+        <div class="w-full bg-gradient-to-br from-[#7a1310] to-[#a31a16] h-[200px] md:h-[240px] flex items-center border-b border-[#7a1310]/20 shadow-inner">
+            <div class="max-w-7xl mx-auto px-6 w-full text-center">
+                <div data-aos="fade-up" data-aos-duration="800">
+                    <h2 class="text-2xl md:text-4xl font-black text-white tracking-tight mb-3 drop-shadow-sm line-clamp-2 max-w-4xl mx-auto">
+                        <?php echo $titulo_articulo; ?>
+                    </h2>
+                    <div class="text-xs md:text-sm font-bold text-white/70 tracking-wide">
+                        <a href="index.php" class="hover:text-white transition-colors">Inicio</a> 
+                        <i class="fa-solid fa-angle-right text-[10px] mx-2 opacity-50"></i> 
+                        <a href="educacion-financiera.php" class="hover:text-white transition-colors">Educación Financiera</a>
+                        <i class="fa-solid fa-angle-right text-[10px] mx-2 opacity-50"></i>
+                        <span class="text-[#ffd9d6] font-extrabold">Lectura de Artículo</span>
                     </div>
                 </div>
-                <div class="row d-flex align-items-center">
-                     <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
-                        <!--===============spacing==============-->
-                        <div class="pd_top_80"></div>
-                        <!--===============spacing==============-->
-                        
-                        <!--===============spacing==============-->
-                        <div class="pd_bottom_80"></div>
-                        <!--===============spacing==============-->
-                     </div>
-                     <div class="col-lg-4 hidden-md image_column">
-                        <div class="slider_image margin_extra" style="position: absolute;
-                     text-align: right;margin: -250px -158px -330px 0px !important;">
-                           <img style="max-width: 45%;
-                         height: auto;" src="assets/img/all-images/about/cal-img.png" class="img-fluid" alt="slider image">
-                        </div>
-                     </div>
-                  </div>
+            </div>
+        </div>
+
+        <!-- ===== CUERPO DEL ARTÍCULO COMPARTIDO ===== -->
+        <section class="py-16 md:py-24 max-w-7xl mx-auto px-6">
+            <div class="grid grid-cols-1 lg:grid-cols-12 gap-12 items-start">
                 
-            </div>
-        </div>
-        <!--===== HERO AREA ENDS =======-->
-
-<!--===== BLOG AREA STARTS =======-->
-<div class="blog-details-siderbars-area sp8">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8">
-                <div class="blog-main-detailsarea rightpadding">
-                    <h3><?php echo arreglar_mojibake(utf8_encode($deteducafinan[0]['titulo_edfi'])) ?></h3>
-                    <div class="space16"></div>
-                    <p><?php echo arreglar_mojibake(utf8_encode($deteducafinan[0]['resumen_edfi'])) ?></p>
-                    <div class="space32"></div>
-                    <div class="img1">
-                        <img src="assets/img/<?php echo utf8_encode($deteducafinan[0]['imagen_edfi']) ?>" alt="">
-                    </div>
-                    <div class="space32"></div>
-                    <ul class="list-author">
-                        <li><a href="#"><img src="assets/img/all-images/blog/blog-img21.png" alt="">  Administrador<span> | </span></a></li>
-                        <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                            <path d="M5.83333 9.16699H7.5V10.8337H5.83333V9.16699ZM17.5 5.00032V16.667C17.5 17.5837 16.75 18.3337 15.8333 18.3337H4.16667C3.72464 18.3337 3.30072 18.1581 2.98816 17.8455C2.67559 17.5329 2.5 17.109 2.5 16.667L2.50833 5.00032C2.50833 4.08366 3.24167 3.33366 4.16667 3.33366H5V1.66699H6.66667V3.33366H13.3333V1.66699H15V3.33366H15.8333C16.75 3.33366 17.5 4.08366 17.5 5.00032ZM4.16667 6.66699H15.8333V5.00032H4.16667V6.66699ZM15.8333 16.667V8.33366H4.16667V16.667H15.8333ZM12.5 10.8337H14.1667V9.16699H12.5V10.8337ZM9.16667 10.8337H10.8333V9.16699H9.16667V10.8337Z" fill="#333535"/>
-                          </svg>  <?php echo utf8_encode($deteducafinan[0]['fecha_edfi']) ?><span> | </span></a></li>
-                          <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                            <path d="M17.841 9.65008L10.341 2.15008C10.0285 1.84015 9.60614 1.6664 9.16602 1.66675H3.33268C2.89066 1.66675 2.46673 1.84234 2.15417 2.1549C1.84161 2.46746 1.66602 2.89139 1.66602 3.33342V9.16675C1.66584 9.38668 1.7092 9.60446 1.79358 9.80756C1.87796 10.0106 2.00171 10.195 2.15768 10.3501L9.65768 17.8501C9.97017 18.16 10.3926 18.3338 10.8327 18.3334C11.274 18.3316 11.6966 18.1547 12.0077 17.8417L17.841 12.0084C18.154 11.6973 18.3308 11.2747 18.3327 10.8334C18.3329 10.6135 18.2895 10.3957 18.2051 10.1926C18.1207 9.98952 17.997 9.80513 17.841 9.65008ZM10.8327 16.6667L3.33268 9.16675V3.33342H9.16602L16.666 10.8334M5.41602 4.16675C5.66324 4.16675 5.90492 4.24006 6.11048 4.37741C6.31604 4.51476 6.47626 4.70999 6.57087 4.93839C6.66547 5.1668 6.69023 5.41813 6.642 5.66061C6.59377 5.90309 6.47472 6.12582 6.2999 6.30063C6.12508 6.47545 5.90236 6.5945 5.65988 6.64273C5.4174 6.69096 5.16607 6.66621 4.93766 6.5716C4.70925 6.47699 4.51403 6.31677 4.37668 6.11121C4.23933 5.90565 4.16602 5.66398 4.16602 5.41675C4.16602 5.08523 4.29771 4.76729 4.53213 4.53287C4.76655 4.29844 5.0845 4.16675 5.41602 4.16675Z" fill="#333535"/>
-                          </svg> Finanzas <span> | </span></a></li>
+                <!-- COLUMNA PRINCIPAL DE LECTURA (Ancho 8 de 12) -->
+                <div class="lg:col-span-8 space-y-6" data-aos="fade-right">
+                    <div class="bg-neutral-50 rounded-[32px] p-6 md:p-8 border border-neutral-100 shadow-soft">
+                        <h3 class="text-xl md:text-2xl font-black text-neutral-800 tracking-tight mb-4"><?php echo $titulo_articulo; ?></h3>
+                        <p class="text-neutral-500 text-xs md:text-sm leading-relaxed text-justify font-medium mb-6"><?php echo $resumen_articulo; ?></p>
                         
-                    </ul>
-                    <div class="space32"></div>
-                    <?php echo arreglar_mojibake(utf8_encode($deteducafinan[0]['descripcion_edfi'])) ?>
-                    <div class="tags-social">
-                        <div class="tags">
-                            <ul>
-                                <li>Tags:</li>
-                                <li><a href="#">Finanzas</a></li>
-                                <li><a href="#">Desarrollo</a></li>
-                                <li><a href="#" class="m-0">Creativo</a></li>
-                            </ul>
+                        <!-- Caja de Imagen Adaptativa Fluida -->
+                        <div class="w-full rounded-2xl overflow-hidden shadow-md border border-neutral-200/40 p-1 bg-white mb-6">
+                            <img src="assets/img/<?php echo $imagen_articulo; ?>" alt="Taller" class="w-full h-auto rounded-xl object-contain bg-white mx-auto">
                         </div>
-                        <div class="social">
-                            <ul>
-                                <li>Compartir:</li>
-                                <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
-                                <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
-                                <li><a href="#" class="m-0"><i class="fa-brands fa-youtube"></i></a></li>
-                            </ul>
+
+                        <!-- Metadatos e Iconos Vectoriales corregidos de FontAwesome -->
+                        <div class="flex flex-wrap gap-5 text-[11px] font-bold text-neutral-400 uppercase tracking-wider mb-6 pb-4 border-b border-neutral-100">
+                            <span class="flex items-center gap-1.5"><i class="fa-regular fa-user text-[#a31a16] text-xs"></i> Por: Administrador</span>
+                            <span class="text-neutral-200">|</span>
+                            <span class="flex items-center gap-1.5"><i class="fa-regular fa-calendar-days text-[#a31a16] text-xs"></i> Publicado: <?php echo $fecha_articulo; ?></span>
+                            <span class="text-neutral-200">|</span>
+                            <span class="flex items-center gap-1.5"><i class="fa-solid fa-graduation-cap text-[#a31a16] text-xs"></i> Taller Finanzas</span>
+                        </div>
+
+                        <!-- Bloque de contenido enriquecido saneado de tildes -->
+                        <div class="text-neutral-600 text-xs md:text-sm leading-relaxed text-justify font-medium prose max-w-none [&_p]:mb-4 [&_strong]:text-neutral-800">
+                            <?php echo $contenido_articulo; ?>
                         </div>
                     </div>
-                    <div class="space32"></div>
+                    <!-- Bloque de Compartir e Interacción en Redes -->
+                    <div class="flex flex-col sm:flex-row justify-between items-center bg-neutral-50 rounded-2xl p-4 border border-neutral-100 gap-4 text-xs font-black">
+                        <div class="flex items-center gap-2 text-neutral-400 uppercase tracking-wider">
+                            <i class="fa-solid fa-tags text-[#a31a16]"></i> Tags: <span class="text-neutral-600 bg-white px-2.5 py-1 rounded-lg border border-neutral-100">Finanzas</span> <span class="text-neutral-600 bg-white px-2.5 py-1 rounded-lg border border-neutral-100">Desarrollo</span>
+                        </div>
+                        <div class="flex items-center gap-3 text-neutral-500">
+                            <span class="uppercase tracking-wider text-neutral-400">Compartir:</span>
+                            <a href="#" class="w-7 h-7 rounded-lg bg-white border border-neutral-100 text-neutral-400 hover:bg-[#a31a16] hover:text-white flex items-center justify-center transition-all shadow-sm"><i class="fa-brands fa-facebook-f text-[10px]"></i></a>
+                            <a href="#" class="w-7 h-7 rounded-lg bg-white border border-neutral-100 text-neutral-400 hover:bg-[#a31a16] hover:text-white flex items-center justify-center transition-all shadow-sm"><i class="fa-brands fa-instagram text-[10px]"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- COLUMNA LATERAL DERECHA (Ancho 4 de 12) -->
+                <div class="lg:col-span-4 space-y-6" data-aos="fade-left" data-aos-delay="150">
                     
-                </div>
-            </div>
-
-            <div class="col-lg-4">
-                <div class="blog-side-widget">
-                    <div class="author-area">
-                        <h3>Autor</h3>
-                        <div class="space24"></div>
-                        <ul>
-                            <li><a href="#"><img src="assets/img/all-images/blog/blog-img20.png" alt=""></a></li>
-                        </ul>
+                    <!-- Widget: Autor -->
+                    <div class="bg-neutral-50 rounded-[28px] p-6 border border-neutral-100 shadow-soft text-center">
+                        <h4 class="text-sm font-black text-neutral-800 tracking-tight mb-4 uppercase tracking-wider pb-2 border-b border-neutral-100">Autor Corporativo</h4>
+                        <div class="w-20 h-24 rounded-2xl overflow-hidden mx-auto border border-neutral-200/50 p-1 bg-white mb-3 shadow-inner-sm">
+                            <img src="assets/img/all-images/blog/blog-img20.png" class="w-full h-full object-cover rounded-xl" alt="Autor">
+                        </div>
+                        <p class="text-xs font-black text-neutral-700 uppercase tracking-wide">Departamento de TI</p>
+                        <p class="text-[11px] text-neutral-400 font-bold mt-0.5">COAC 15 de Agosto</p>
                     </div>
-                    <div class="space30"></div>
-                    <div class="social-area">
-                        <h3>Síguenos en</h3>
-                        <div class="space24"></div>
-                        <ul>
-                            <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
-                            <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
-                            <li><a href="#"><i class="fa-brands fa-pinterest-p"></i></a></li>
-                            <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
-                        </ul>
+
+                    <!-- Widget: Síguenos -->
+                    <div class="bg-neutral-50 rounded-[28px] p-6 border border-neutral-100 shadow-soft text-center">
+                        <h4 class="text-sm font-black text-neutral-800 tracking-tight mb-4 uppercase tracking-wider pb-2 border-b border-neutral-100">Síguenos en Línea</h4>
+                        <div class="flex justify-center gap-3">
+                            <a href="#" class="w-9 h-9 rounded-xl bg-white border border-neutral-100 text-neutral-400 hover:bg-[#a31a16] hover:text-white flex items-center justify-center transition-all shadow-sm"><i class="fa-brands fa-facebook-f text-sm"></i></a>
+                            <a href="#" class="w-9 h-9 rounded-xl bg-white border border-neutral-100 text-neutral-400 hover:bg-[#a31a16] hover:text-white flex items-center justify-center transition-all shadow-sm"><i class="fa-brands fa-instagram text-sm"></i></a>
+                            <a href="#" class="w-9 h-9 rounded-xl bg-white border border-neutral-100 text-neutral-400 hover:bg-[#a31a16] hover:text-white flex items-center justify-center transition-all shadow-sm"><i class="fa-brands fa-youtube text-sm"></i></a>
+                        </div>
                     </div>
+
                 </div>
+
             </div>
-        </div>
-    </div>
-</div>
-<div style="height: 150px;"></div>
+        </section>
 
-
-<!--===== BLOG AREA ENDS =======-->
-
-        <!--===== FOOTER AREA STARTS =======-->
+        <!--===== FOOTER AREA =======-->
         <div class="vl-footer2-section-area">
             <?php include './footer.php' ?>
         </div>
-        <!--===== FOOTER AREA ENDS =======-->
 
         <?php include "scripts-v2.php"; ?>
-</body>
+    </body>
 </html>
